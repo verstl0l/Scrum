0cef46cbd4fbeb2faf7da1a610eb68cf88b40c90
verstl0l <victor.rodrigues0408@gmail.com>
2025-08-25 13:10:13 -0300

    Update Cod.layout.py

--- Cod.layout.py
+++ Cod.layout.py
@@ -1,5 +1,31 @@
 import tkinter as tk
 
+expressao = ""
+
+def adicionar_valor(valor):
+    global expressao
+    expressao += str(valor)
+    entrada.delete(0, tk.END) 
+    entrada.insert(0, expressao) 
+
+def calcular():
+    global expressao
+    try:
+        resultado = str(eval(expressao))
+        entrada.delete(0, tk.END)
+        entrada.insert(0, resultado)
+        expressao = resultado
+    except:
+        entrada.delete(0, tk.END)
+        entrada.insert(0, "Erro")
+        expressao = ""
+
+# Função para limpar a tela
+def limpar():
+    global expressao
+    expressao = ""
+    entrada.delete(0, tk.END)
+
 janela = tk.Tk()
 janela.title("Calculadora")
 
@@ -15,13 +41,15 @@ botoes = [
 
 row_val = 1
 col_val = 0
-
 for botao in botoes:
- 
-    tk.Button(janela, text=botao, padx=20, pady=20).grid(row=row_val, column=col_val)
+    if botao == '=':
+        tk.Button(janela, text=botao, padx=20, pady=20, command=calcular).grid(row=row_val, column=col_val)
+    elif botao == 'C':
+        tk.Button(janela, text=botao, padx=20, pady=20, command=limpar).grid(row=row_val, column=col_val)
+    else:
+        tk.Button(janela, text=botao, padx=20, pady=20, command=lambda b=botao: adicionar_valor(b)).grid(row=row_val, column=col_val)
     
     col_val += 1
-
     if col_val > 3:
         col_val = 0
         row_val += 1
